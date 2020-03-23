f1389c78f2e5c947e72faa95854be2a39ace4307
Tyler Berrett <btylerberrett@gmail.com>
2020-03-23 15:17:02 -0600

    add functionality to run in cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256  # the commands
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +70,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010  # 130
+        PRN = 0b01000111  # 71
+        HLT = 0b00000001  # 1
+
+        running = True
+
+        while running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == HLT:
+                running = False
+            else:
+                print(f"Invalid command: {IR}")
+                quit()
+
+
+
+
+
+
